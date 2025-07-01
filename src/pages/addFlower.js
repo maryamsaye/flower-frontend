1f68fd6034e9ee1c16c16de6359a69b29a4581cf
maryamsaye <marynsaye@gmail.com>
2025-07-01 17:47:49 +0100

    form-data3

--- src/pages/addFlower.js
+++ src/pages/addFlower.js
@@ -6,8 +6,6 @@ import './addFlower.css';
 
 const AddFlower = () => {
 
-  console.log('BODY ➜', req.body);
-  console.log('FILE ➜', req.file);
 
   const [formData, setFormData] = useState({
     title: '',
